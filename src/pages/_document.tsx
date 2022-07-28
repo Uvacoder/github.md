20f9eaf24738c6e2852855c4949fc1bfb791bf8f
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-07-28 08:14:25 +0300

    Split home page into small components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, {
   Main,
   NextScript,
   DocumentContext,
-} from "next/document";
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -12,6 +12,7 @@ class MyDocument extends Document {
     return { ...initialProps };
   }
 
+  // Prevent theme change flashing on initial load
   render() {
     const setInitialTheme = `
       function getUserPreference() {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,35 +4,59 @@ import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>repo.md</title>
         <meta name="description" content="repo.md" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="grid items-center text-center py-4 place-content-center dark:text-stone-400">
-        <h1 className="text-3xl mb-4 dark:text-stone-100">
-          A place for your markdown projects.
-        </h1>
-        <h2 className="text-xl">View your Github project as a blog.</h2>
-        <div className="flex mx-auto items-center my-20">
-          <ButtonLink size="big" link={{ href: '/gh' }}>
-            Select a project
-          </ButtonLink>
-        </div>
-        <h2 className="text-xl mb-6">
-          🔨 Built with NextJS, TypeScript, Tailwind
-        </h2>
-        <div className="grid gap-4 sm:flex mx-auto items-center">
-          <h2 className="text-xl dark:text-stone-200">Check out our Github</h2>
-          <ButtonLink
-            text="Visit our repo"
-            link={{ href: 'https://github.com/ilyasudakov', newTab: true }}
-          />
-        </div>
+        <Title />
+        <SubTitle />
+        <SelectProject />
+        <BuiltWith />
       </main>
-    </div>
+    </>
   );
 };
 
 export default Home;
+
+const Title: React.FC = () => {
+  return (
+    <h1 className="text-3xl mb-4 dark:text-stone-100">
+      A place for your markdown projects.
+    </h1>
+  );
+};
+
+const SubTitle: React.FC = () => {
+  return <h2 className="text-xl">View your Github project as a blog.</h2>;
+};
+
+const SelectProject: React.FC = () => {
+  return (
+    <div className="flex mx-auto items-center my-20">
+      <ButtonLink size="big" link={{ href: '/gh' }}>
+        Select a project
+      </ButtonLink>
+    </div>
+  );
+};
+
+const BuiltWith: React.FC = () => {
+  return (
+    <>
+      <h2 className="text-xl mb-6">
+        🔨 Built with NextJS, TypeScript, Tailwind
+      </h2>
+      <div className="grid gap-4 sm:flex mx-auto items-center">
+        <h2 className="text-xl dark:text-stone-200">Check out our Github</h2>
+        <ButtonLink
+          text="Visit our repo"
+          link={{ href: 'https://github.com/ilyasudakov', newTab: true }}
+        />
+      </div>
+    </>
+  );
+};
